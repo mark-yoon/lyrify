aa96cbf8c7a8b6069b66990edaa75f871ed13aac
Mark Yoon <mark.youngho.yoon@gmail.com>
2016-10-15 16:15:23 -0700

    Script.js: add better song/artist parsing and UI tweaks
    This commit adds better parsing for song names and artists. It should
    play better with arists/songs with periods in them, songs with featured
    artists, and the A$AP folk. I also tried to use the Musicxmatch API
    but realized that they only provide 30% of the lyrics via the API, qq.
    Either way, this should be good enough for now.
    
    I also chose not to use a loop or setInterval to refresh the lyrics.
    Figured that refreshing the app is easy enough and didn't want to waste
    too much cpu cycles.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 $(function(){
-    var osascript = require('node-osascript');
+  var osascript = require('node-osascript');
 
-    osascript.execute('tell application "Spotify"\n set currentArtist to artist of current track as string\n set currentSong to name of current track as string\n return {currentSong, currentArtist}\n end tell', function(err, result, raw) {
-      song_artist = JSON.stringify(result[1]);
-      song_title = JSON.stringify(result[0]);
-      $('.song').append("Title: " + song_title);
-      $('.artist').append("Artist: " + song_artist);
-      regex_title = song_title.replace(/['"]+/g,'').replace(/\s/g, '').toLowerCase();
-      regex_artist = song_artist.replace(/['"]+/g,'').replace(/\s/g, '').toLowerCase();
-      document.getElementById('lyrics').src = "http://www.azlyrics.com/lyrics/" + regex_artist + "/" + regex_title + ".html";
-    });
+  osascript.execute('tell application "Spotify"\n set currentArtist to artist of current track as string\n set currentSong to name of current track as string\n return {currentSong, currentArtist}\n end tell', function(err, result, raw) {
+    song_title = JSON.stringify(result[0]).slice(1, -1);
+    song_artist = JSON.stringify(result[1]).slice(1, -1);
+    if (song_title.indexOf("(") != -1) {
+      song_title = song_title.substring(0, song_title.indexOf("("));
+    }
+    document.title = song_title + ": " + song_artist;
+    regex_title = song_title.replace(/\./g, '').replace(/\s/g, '').toLowerCase();
+    regex_artist = song_artist.replace(/\$/g, 's').replace(/\./g, '').replace(/\s/g, '').toLowerCase();
+    document.getElementById('lyrics').src = "http://www.azlyrics.com/lyrics/" + regex_artist + "/" + regex_title + ".html";
+  });
 });
