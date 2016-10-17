2ab5236cb774b7afce35bc4f40b3091b80b3dc91
Mark Yoon <mark.youngho.yoon@gmail.com>
2016-10-16 22:41:02 -0700

    Test setting domain for getting response on azlyrics

--- js/script.js
+++ js/script.js
@@ -20,19 +20,7 @@ $(function(){
   //    };
   //  });
   //};
-  setInterval( function() {
-    osascript.execute('tell application "Spotify"\n set currentArtist to artist of current track as string\n set currentSong to name of current track as string\n return {currentSong, currentArtist}\n end tell', function(err, result, raw) {
-      song_title = JSON.stringify(result[0]).slice(1, -1);
-      song_artist = JSON.stringify(result[1]).slice(1, -1);
-      if (song_title.indexOf("(") != -1) {
-        song_title = song_title.substring(0, song_title.indexOf("("));
-      }
-      if (document.title != song_title + ": " + song_artist) {
-        document.title = song_title + ": " + song_artist;
-        regex_title = song_title.replace(/\./g, '').replace(/\s/g, '').toLowerCase();
-        regex_artist = song_artist.replace(/\$/g, 's').replace(/\./g, '').replace(/\s/g, '').toLowerCase();
-        document.getElementById('lyrics').src = "http://www.azlyrics.com/lyrics/" + regex_artist + "/" + regex_title + ".html";
-      };
-    });
-  }, 1000);
+  document.domain = "azlyrics.com";
+  var a = $.get("http://www.azlyrics.com/lyrics/greenday/somewherenow.html");
+  document.title = a.responseText;
 });
